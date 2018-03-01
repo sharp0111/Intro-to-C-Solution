2df469faa11a77ff19d7cddc13a1fda7bab5939e
Sean Chen <skypemaster007@gmail.com>
2018-03-01 10:25:59 -0800

    Banging out READMEs

--- fizzbuzz/src/fizzbuzz.c
+++ fizzbuzz/src/fizzbuzz.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+    Your fizzbuzz implementation should initialize a counter, then
+    iterate n times. If the ith iteration is divisible by 3, print
+    "Fizz". If it's divisible by 5, print "Buzz". If it's divisible
+    by both 3 and 5, print "FizzBuzz". Increment the counter variable
+    every time that nothing gets printed and return the counter.
+*/
 int fizzbuzz(int n)
 {
     int count = 0;
@@ -9,11 +16,15 @@ int fizzbuzz(int n)
             printf("Fizz");
         }
 
-        if (i % 5 == 0) {
+        else if (i % 5 == 0) {
             printf("Buzz");
         }
 
-        if (i % 3 && i % 5) {
+        else if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz");
+        }
+
+        else {
             count++;
         }
 
