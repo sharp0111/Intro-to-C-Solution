9ce7c9838a05f03807abc994b28b6e54c640f971
Sean Chen <skypemaster007@gmail.com>
2018-03-02 11:01:50 -0800

    Write up README for malloc module

--- malloc/src/malloc.c
+++ malloc/src/malloc.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
+#include "../../pointers/src/pointers.h"
 
 /*
     Duplicates the input string by dynamically allocating memory for 
     the duplicate string using `malloc` and then copying the string
     into the allocated memory. Returns a pointer to the allocated memory.
+    You may want to use the string_length function to figure out the
+    length of the input string.
 */
 char *string_dup(char *src)
 {
     char *str;
     char *p;
-    int len = 0;
-
-    while (src[len]) {
-        len++;
-    }
+    int len = string_length(src);
 
     str = malloc(len + 1);
     p = str;
@@ -29,6 +28,8 @@ char *string_dup(char *src)
 /*
     A generic version of string_copy, mem_copy receives a block of memory
     of any type and copies its contents to the destination pointer (dest).
+    You may want to cast the input pointers to char pointers first before
+    performing the copying. 
 */
 void *mem_copy(void *dest, const void *src, int n)
 {
