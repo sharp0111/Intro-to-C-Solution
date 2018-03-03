3b1a7abcbad09088df04b790c1879b582e42c2fd
Sean Chen <skypemaster007@gmail.com>
2018-03-03 10:47:07 -0800

    Alter reverse_string function to accept an input array as a parameter

--- strings/strings.c
+++ strings/strings.c
@@ -18,15 +18,14 @@ int string_length(char s[])
 
 /*
     Don't worry about this funky 'char *' going on here. We'll get to
-    that shortly. In the meantime, write a function that allocates a
-    new char array with the capacity given by the length of the input
-    string and add the characters from the input string into the new
-    array in reverse order. Return the new array.
+    that shortly. In the meantime, write a function that reverses the 
+    order of string s and outputs the reversed string to the input 
+    array rv. The rv array will have enough space for the reversed 
+    string. Return the rv array.
 */
-char *reverse_string(char s[])
+char *reverse_string(char rv[], char s[])
 {
     int n = string_length(s);
-    char rv[n+1];
 
     for (int i = 0; i < n; i++) {
         rv[i] = s[n-i-1];
@@ -36,33 +35,16 @@ char *reverse_string(char s[])
     return rv;
 }
 
-/* 
-    Better solution that utilizes malloc so the rv array persists
-    past this function's scope. However, the students haven't 
-    been exposed to `malloc` yet at this point.
-*/
-// char *reverse_string(char s[])
-// {
-//     int n = string_length(s);
-//     char *rv = malloc(n+1); // +1 to make room for \0
-
-//     for (int i = 0; i < n; i++) {
-//         rv[i] = s[n - i - 1];
-//     }
-
-//     rv[n] = '\0';
-
-//     return rv;
-//}
-
 #ifndef TESTING
 int main(void)
 {
     char quote1[] = "Don't forget to be awesome";
     char quote2[] = "a man a plan a canal panama";
 
+    char rv[512];
+
     printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
-    printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(quote2));
+    printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
 
     return 0;
 }
