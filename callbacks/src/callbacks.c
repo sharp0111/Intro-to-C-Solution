fc00ce7816362a54b795ec6938d809b59e7d5208
Sean Chen <skypemaster007@gmail.com>
2018-03-02 16:23:54 -0800

    Finish up function pointers README

--- callbacks/src/callbacks.c
+++ callbacks/src/callbacks.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <callbacks.h>
 #include "../../malloc/src/malloc.h"
+#include "../../pointers/src/pointers.h" 
 
 /*
     Implement the callback that will be passed to your bubble_sort function to sort 
@@ -23,20 +24,18 @@ int reverse_order(int a, int b)
 /*
     Receives an array of numbers to be sorted, the number of elements in the array,
     and a callback that specifies the order in which the numbers will be sorted.
+    Your function should allocate memory to hold the sorted data and return the 
+    sorted data. Call the callback as you would any other function. 
 */
 int *bubble_sort(int *numbers, int n, compare_cb cmp)
 {
-    int temp;
     int *target = malloc(n * sizeof(int));
-    
     mem_copy(target, numbers, n * sizeof(int));
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - 1; j++) {
             if (cmp(target[j], target[j + 1]) > 0) {
-                temp = target[j + 1];
-                target[j + 1] = target[j];
-                target[j] = temp;
+                swap(&target[j], &target[j+1]);
             }
         }
     }
